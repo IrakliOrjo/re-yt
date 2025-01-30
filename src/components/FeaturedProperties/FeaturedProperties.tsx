1620f3e31885381905e3cf099d6c34634c74d9be
Irakli <irakliorjo92@gmail.com>
2025-01-30 23:45:03 +0400

    fixed input button

--- src/components/FeaturedProperties/FeaturedProperties.tsx
+++ src/components/FeaturedProperties/FeaturedProperties.tsx
@@ -12,7 +12,7 @@ export const FeaturedProperties = () => {
     <div className="flex flex-col justify-center items-center py-20 px-5">
         <p className="text-blue-600 xl:text-[20px] text-[14px] font-[500] uppercase">Featured Properties</p>
         <h1 className="xl:text-[34px] text-[1.6rem] mb-9 font-bold">Recommended For You</h1>
-        <div className="xl:grid xl:grid-cols-3 flex flex-col gap-6 w-full">
+        <div className="xl:grid xl:grid-cols-3 flex flex-col md:grid md:grid-cols-2 gap-6 w-full">
             {filteredHouses.map((house) => {
                 return (
                     <HouseCard
